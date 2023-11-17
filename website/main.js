02d83abc0d88126794c675e1bcf783dae58a068b
Deaths-Door <aaravaditya51@gmail.com>
2023-11-17 17:14:06 +0100

    Added list to website

--- website/main.js
+++ website/main.js
@@ -9,7 +9,34 @@ document.querySelector('button').addEventListener('click',() => {
 
     // TODO : Convert number
     try { 
-        result.innerText = StandardForm.new_from_string(number).into_f64() 
+        const sf = StandardForm.new_from_string(number).into_f64();
+        result.innerText = sf;
+
+        let history =  document.getElementById('history');
+        const text = number + " = " + sf;
+
+        if (history.lastElementChild?.textContent == text){
+            return
+        }
+
+        let item = document.createElement("li");
+
+        const spanCreator = (part,sep) => {
+            var span = document.createElement('span');
+            span.textContent = part;
+
+            if (sep) {
+                span.classList.add('separator');
+            }
+
+            item.appendChild(span);   
+        };
+        
+        spanCreator(number,false);
+        item.appendChild(document.createTextNode(' = '),true);
+        spanCreator(sf,false);
+    
+        history.appendChild(item)
     } catch(err) {
         result.innerText = "";
         title.innerText = err;
